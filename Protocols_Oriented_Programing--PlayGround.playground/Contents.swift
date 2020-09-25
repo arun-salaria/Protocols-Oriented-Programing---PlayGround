b7d54018f692beb3ead3cd098fc7e37f12c9305c
arun-salaria <arun.s.salaria@gmail.com>
2020-09-25 23:31:50 +0530

    POP complete without commenting

--- Protocols_Oriented_Programing--PlayGround.playground/Contents.swift
+++ Protocols_Oriented_Programing--PlayGround.playground/Contents.swift
@@ -1,3 +1,68 @@
-import UIKit
+//Just a simple and basic example for POP
 
-var str = "Hello, playground"
+protocol Person {
+    var name:String {get}
+    var isMinor:Bool {get}
+}
+
+protocol Student:Person {
+    var numberOfSubjects:Int{get}
+    var completed10th:Bool{get}
+}
+
+protocol Teacher:Person {
+    var subjectName:String{get}
+}
+
+extension Person {
+    var isMinor :Bool {self is Student}
+}
+
+extension Student {
+    var completed10th:Bool {self is NonMedSutdent}
+}
+
+struct Tuitor:Person,Teacher {
+    var name: String
+    var subjectName: String
+    
+}
+
+struct NonMedSutdent :Student,Person {
+    var numberOfSubjects: Int
+    var name: String
+}
+
+enum BestTeacher:Teacher {
+    
+    case arun
+    case shubham
+    
+    var name:String {
+        switch self {
+        case .arun:
+            return "Arun"
+        case .shubham:
+            return "Shubham"
+        }
+    }
+    var subjectName:String {
+        switch self {
+        case .arun:
+            return "Maths"
+        case .shubham:
+            return "English"
+        }
+    }
+    
+}
+
+BestTeacher.shubham.name
+BestTeacher.arun.isMinor
+let shivani = NonMedSutdent(numberOfSubjects: 5, name: "Shivani")
+shivani.numberOfSubjects
+shivani.isMinor
+let rama = Tuitor(name: "Rama", subjectName: "Hindi")
+rama.isMinor
+
+
